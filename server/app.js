a01926a915a624431c80e8b46c962ed1e27481f6
Ayinla Abeeb <abeebayinla@gmail.com>
2022-02-06 14:22:37 +0100

    Added not-found route message

--- server/app.js
+++ server/app.js
@@ -15,5 +15,13 @@ app.use(morgan('dev'));
 app.use('/api/products', productRoute);
 app.use('/api/carts', cartRoute);
 
+app.all('*', (req, res) => {
+    return res.status(404).json({
+        status: 'fail',
+        message: `resource not found on the server!`,
+        code: 404,
+    });
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App listening on port ${port}`));
