cb883e39ff18650b927571ee11a0d94a4d59a317
Ayinla Abeeb <abeebayinla@gmail.com>
2022-02-07 08:39:15 +0100

    Implemented add to cart functionality

--- web/src/views/Cart.vue
+++ web/src/views/Cart.vue
@@ -1,13 +1,182 @@
 <template>
-  <h4>Cart page</h4>
+    <div class="container-fluid">
+        <div class="row mb-5">
+            <div class="col-sm-12 col-md-8">
+                <div v-if="carts">
+                    <div class="card shadow-sm" style="width: 100%">
+                        <table class="table">
+                            <tbody>
+                                <tr v-for="cart in carts" :key="cart._id">
+                                    <td>
+                                        <img
+                                            :src="cart.product.image"
+                                            :alt="`${cart.product.name} Image`"
+                                            style="width: 30%"
+                                        />
+                                    </td>
+                                    <td>{{ cart.product.name }}</td>
+                                    <td>{{ cart.product.description }}</td>
+                                    <td>{{ cart.product.category }}</td>
+                                    <td
+                                        class="d-flex justify-content-between align-items-center"
+                                    >
+                                        {{ cart.quantity }}
+                                        <!-- <Button
+                                            @click="decrementQuantity"
+                                            class="btn-secondary font-weight-bold"
+                                        >
+                                            -
+                                        </Button>
+                                        <div class="p-1">
+                                            {{ cart.quantity }}
+                                        </div>
+                                        <Button
+                                            @click="incrementQuantity"
+                                            class="btn-secondary font-weight-bold"
+                                        >
+                                            +
+                                        </Button> -->
+                                    </td>
+                                    <td>
+                                        {{
+                                            cart.product.price.toLocaleString(
+                                                'en-US',
+                                                {
+                                                    style: 'currency',
+                                                    currency: 'USD',
+                                                }
+                                            )
+                                        }}
+                                    </td>
+                                    <td>
+                                        {{
+                                            cart.totalPrice.toLocaleString(
+                                                'en-US',
+                                                {
+                                                    style: 'currency',
+                                                    currency: 'USD',
+                                                }
+                                            )
+                                        }}
+                                    </td>
+                                    <td>
+                                        <Button class="btn-danger"
+                                            >&times;</Button
+                                        >
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div
+                        class="d-flex justify-content-end font-weight-bold mt-4"
+                    >
+                        <router-link to="/" class="btn btn-sm btn-dark"
+                            >Continue shopping</router-link
+                        >
+                    </div>
+                </div>
+                <div v-else>
+                    <p class="text-muted text-center card shadow-sm">
+                        No items in your bag
+                    </p>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4">
+                <div class="card shadow-sm" style="width: 100%">
+                    <div class="card-body card-content">
+                        <h5 class="card-title font-weight-bold">TOTAL</h5>
+                        <hr />
+                        <div
+                            class="d-flex justify-content-between font-weight-bold"
+                        >
+                            <p class="card-text">Sub-total</p>
+                            <p class="card-text text-muted">$300.00</p>
+                        </div>
+                        <div
+                            class="d-flex justify-content-between font-weight-bold"
+                        >
+                            <p class="card-text">Delivery</p>
+                            <p class="card-text text-muted">None</p>
+                        </div>
+                        <Button class="btn-block btn-success font-weight-bold"
+                            >CHECKOUT</Button
+                        >
+                        <h6 class="card-title font-weight-bold mt-3">
+                            WE ACCEPT
+                        </h6>
+                        <div
+                            class="payment-icons d-flex justify-content-between mt-4"
+                        >
+                            <p>Icon 1</p>
+                            <p>Icon 2</p>
+                            <p>Icon 3</p>
+                            <p>Icon 4</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            carts: [],
+            isLoading: false,
+            subTotal: null
+        };
+    },
 
-}
+    methods: {
+        async loadCarts() {
+            this.isLoading = true;
+            try {
+                let result = await fetch(`http://localhost:3100/api/carts`);
+                if (!result.ok) {
+                    this.isLoading = false;
+                    return this.$notify(
+                        {
+                            group: 'error',
+                            title: 'Error',
+                            text: 'An error occured! Please retry',
+                        },
+                        5000
+                    );
+                } else {
+                    let response = await result.json();
+                    let data = response.data.carts;
+                    this.carts = data;
+                }
+            } catch (err) {
+                this.isLoading = false;
+                return this.$notify(
+                    {
+                        group: 'error',
+                        title: 'Error',
+                        text: err.message,
+                    },
+                    5000
+                );
+            }
+        },
+
+        async calculateSubTotal() {
+          // let a = this.carts.map((el) => {
+          //   console.log(el.totalPrice)
+          // })
+          console.log(this.carts);
+        },
+
+    },
+
+    mounted() {
+        this.loadCarts();
+        this.calculateSubTotal();
+    },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
